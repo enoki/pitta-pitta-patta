f9bc91ec1e9938dfac0b4c9b1f5701bb73da0519
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-17 13:32:14 +0000

    Put separate classes into separate files.

---
+++ trunk/HomePile.py
@@ -0,0 +1,21 @@
+#
+# Pitta-pitta-patta
+# Released under the GPL version 2.0 or later.
+#
+
+from Pile import Pile
+
+
+class HomePile(Pile):
+    """ The cards the player must get rid of in order to stop the game. """
+    def __init__(self):
+        Pile.__init__(self)
+        self.initial_size = 13
+
+    def take_from(self, deck):
+        """ Take cards from the deck. Flip over the top card. """
+        for i in range(self.initial_size):
+            self.cards.add_card(deck.take_top_card())
+
+        self.cards.top_card().flip()
+
---
+++ trunk/StockPile.py
@@ -0,0 +1,21 @@
+#
+# Pitta-pitta-patta
+# Released under the GPL version 2.0 or later.
+#
+
+from Pile import Pile
+
+class StockPile(Pile):
+    """ The cards the player holds in his hand during play. """
+
+    def __init__(self):
+        Pile.__init__(self)
+
+    def take_from(self, deck):
+        """ Take all cards from the deck. Flip them over as well. """
+        while not deck.empty():
+            card = deck.take_top_card()
+            card.backSide()
+            self.cards.add_card(card)
+
+
