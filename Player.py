2d2333be02e37c932ea7ef18ebfb68a95a7cb625
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-21 08:37:18 +0000

    Reorganize how cards are positioned for clarity.

--- Player.py
+++ Player.py
@@ -79,16 +79,26 @@ class Player:
 
     def set_locations(self):
         """ Position cards """
-        card_rect = self.home_pile.top_card().rect
-        card_width, card_height = card_rect.width, card_rect.height
+        some_card = self.home_pile.top_card()
+        card_width, card_height = some_card.rect.width, some_card.rect.height
         left_margin, top_margin = 10, 480
-        hand_top_margin = top_margin + card_rect.height + 30
+        hand_top_margin = top_margin + card_height + 30
 
-        self.home_pile.move_to(left_margin, top_margin)
-        self.stock_pile.move_to(left_margin + card_width * 2, hand_top_margin)
-        self.discard_pile.move_to(left_margin + card_width * 2  + card_width * 1.5, hand_top_margin)
-        self.cell_cards.move_to(left_margin + card_width * 2, top_margin, card_width * 1.5)
-        self.right_hand.move_to(left_margin + card_width * 2 + card_width * 1.5 * 2, hand_top_margin, 40)
+        x, y = left_margin, top_margin
+
+        # Top row
+        self.home_pile.move_to(x, y)
+        x += card_width * 2
+        self.cell_cards.move_to(x, y, card_width * 1.5)
+
+        y = hand_top_margin
+
+        # Bottom row
+        self.stock_pile.move_to(x, y)
+        x += card_width * 1.5
+        self.discard_pile.move_to(x, y)
+        x += card_width * 1.5
+        self.right_hand.move_to(x, y, 40)
 
 
     def update(self):
