6179ba7703a0d4dab3767eb1794c921b3555229d
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-22 03:01:38 +0000

    Get the game over screen working again. This time the code is much prettier.

--- trunk/GameOverState.py
+++ trunk/GameOverState.py
@@ -6,6 +6,7 @@
 import pygame
 import louie
 from Color import Color
+from Label import Label
 from State import State
 
 class GameOverState(State):
@@ -15,18 +16,74 @@ class GameOverState(State):
 
     def __init__(self, playing_field):
         self.playing_field = playing_field
+        self.informative = False
+
+        font = pygame.font.SysFont("Arial", 24)
+        self.label = Label(font, Color.white, Color.medium_blue)
 
     def delay(self):
-        pass
+        pygame.time.wait(100)
 
     def handle(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                self.toggle_informative()
+            elif event.key == pygame.K_RETURN:
+                self.new_game()
 
     def update(self):
-        pass
+        if self.label.empty():
+            self.create_ui()
+
+    def create_ui(self):
+        text = 'Game over\n'
+        text += '\n'
+
+        winner = self.playing_field.get_winner()
+        assert(winner)
+
+        if winner in self.playing_field.players:
+            text += 'You Win!\n'
+        else:
+            text += winner.get_name() + ' Wins!\n'
+        text += '\n'
+
+        for player in self.playing_field.players:
+            score = player.get_score()
+            name = player.get_name()
+
+            text += name + ': ' + str(score)
+
+            if self.informative:
+                num_good = player.num_good_cards()
+                num_bad = player.num_bad_cards()
+
+                text += '     (' + str(score) + ' = ' + \
+                        str(num_good) + ' - ' + str(num_bad) + 'x2)'
+
+            text += '\n'
+
+        text += '\n'
+        text += 'Press enter to start a new game\n'
+        text += 'Press space for '
+        if self.informative:
+            text += 'less '
+        else:
+            text += 'more '
+        text += 'information\n'
+
+        self.label.set_text(text)
 
     def clear_surface(self, surface):
-        pass
+        surface.fill(Color.bright_green)
 
     def draw(self, surface):
-        pass
+        self.playing_field.draw(surface)
+        self.label.draw(surface)
+
+    def toggle_informative(self):
+        self.informative = not self.informative
+        self.create_ui()
+
+    def new_game(self):
+        louie.send(GameOverState.new_game)
