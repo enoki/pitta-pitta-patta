6bf38b6dc6f6a53c7de482d7209d87d809f6349b
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-23 13:20:40 +0000

    Clarify some code dealing with shared images.

--- trunk/Deck.py
+++ trunk/Deck.py
@@ -7,16 +7,17 @@ from CardImages import CardImages
 from CardGroup import CardGroup
 from Card import Card
 
-class SharedDeck:
+class SharedImages:
+    """ Private class to store shared images. """
     card_images = None
 
 def Deck():
     """ Creates a deck of cards. """
 
-    if not SharedDeck.card_images:
-        SharedDeck.card_images = CardImages()
+    if SharedImages.card_images is None:
+        SharedImages.card_images = CardImages()
 
-    ci = SharedDeck.card_images
+    ci = SharedImages.card_images
 
     cards = []
 
