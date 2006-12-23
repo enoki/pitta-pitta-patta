a4433e0ad334d500a48099efa6ee2c8d02119c16
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-23 13:19:31 +0000

    Share one ghost image for all foundation piles.

--- FoundationPile.py
+++ FoundationPile.py
@@ -6,13 +6,22 @@
 import pygame
 from Pile import Pile
 
+class SharedImages:
+    """ Private class to store shared images. """
+    ghost_image = None
+
 class FoundationPile(Pile):
     """ A pile on which players drop ordered cards. """
 
     def __init__(self):
         Pile.__init__(self)
-        self.ghost_image = pygame.image.load('images/ghost.png')
+        self.ghost_image = self.get_ghost_image()
         self.ghost_image_rect = self.ghost_image.get_rect()
+
+    def get_ghost_image(self):
+        if SharedImages.ghost_image is None:
+            SharedImages.ghost_image = pygame.image.load('images/ghost.png')
+        return SharedImages.ghost_image
 
     def calibrate(self):
         Pile.calibrate(self)
