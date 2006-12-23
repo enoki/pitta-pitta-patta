5801b8ce9e4327ec2815e39fb9cc4cb97f8d4e64
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2006-12-23 15:19:16 +0000

    Add a simple menu to the pause screen.

--- PausedState.py
+++ PausedState.py
@@ -5,7 +5,10 @@
 
 import pygame
 import louie
+import sys
+from Button import Button
 from Color import Color
+from RectContainer import RectContainer
 from State import State
 from Label import Label
 
@@ -16,18 +19,41 @@ class PausedState(State):
 
     def __init__(self, playing_field):
         self.playing_field = playing_field
-        self.font = pygame.font.SysFont("Arial", 36)
-        self.label = Label(self.font, Color.white, Color.medium_blue)
+        self.title_font = pygame.font.SysFont("Arial", 36)
+        self.button_font = pygame.font.SysFont("Arial", 24)
+        self.widget = RectContainer(Color.medium_blue)
 
     def delay(self):
         pygame.time.wait(30)
 
     def handle(self, event):
-        pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                sys.exit()
+
+        self.widget.handle(event)
+
+    def create_ui(self):
+        label = Label(self.title_font, Color.white, Color.medium_blue)
+        label.set_text('Paused')
+        label.set_y(200)
+        
+        resume_button = self.make_button('Resume (Escape)')
+        exit_button = self.make_button('Exit Game')
+
+        louie.connect(self.finish, Button.clicked, resume_button)
+        louie.connect(self.exit, Button.clicked, exit_button)
+
+        self.widget.create_ui([label, resume_button, exit_button])
+
+    def make_button(self, text):
+        return Button(self.button_font,
+                      Color.white, Color.medium_blue, Color.dark_blue,
+                      0, text)
 
     def entered(self):
-        if self.label.empty():
-            self.label.set_text('Paused\nPress escape to resume')
+        if self.widget.empty():
+            self.create_ui()
 
     def update(self):
         pass
@@ -37,8 +63,10 @@ class PausedState(State):
 
     def draw(self, surface):
         self.playing_field.draw(surface)
-        self.label.center_vertically_on(surface)
-        self.label.draw(surface)
+        self.widget.draw(surface)
 
     def finish(self):
         louie.send(PausedState.finished)
+
+    def exit(self):
+        sys.exit()
