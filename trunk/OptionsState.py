3e0c9152b5f3f12e864b94bb613c95c827d9954d
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2007-01-01 01:26:41 +0000

    Add a way to change the computer skill level.

--- trunk/OptionsState.py
+++ trunk/OptionsState.py
@@ -12,6 +12,7 @@ from GameConfig import GameConfig
 from Label import Label
 from RectContainer import RectContainer
 from State import State
+from SkillLevels import *
 
 class OptionsState(State):
     """ The first screen players see. """
@@ -22,8 +23,9 @@ class OptionsState(State):
     def __init__(self):
         self.game_config = GameConfig()
         self.widget = RectContainer(Color.medium_blue)
-        self.font = pygame.font.SysFont("Arial", 24)
+        self.font = pygame.font.SysFont("Arial", 16)
         self.nump_buttons = ButtonGroup()
+        self.skill_buttons = ButtonGroup()
 
     def delay(self):
         pygame.time.wait(100)
@@ -45,21 +47,36 @@ class OptionsState(State):
 
         label_spacer = self.make_label(' ')
         label_spacer0 = self.make_label(' ')
+        label_spacer1 = self.make_label(' ')
+
+        easy_button = self.make_button('Easy')
+        normal_button = self.make_button('Normal')
+        hard_button = self.make_button('Hard')
 
         finished_button = self.make_button('Finished')
 
         louie.connect(self.set_2p, Button.clicked, two_p_button)
         louie.connect(self.set_3p, Button.clicked, three_p_button)
         louie.connect(self.set_4p, Button.clicked, four_p_button)
+        louie.connect(self.set_easy, Button.clicked, easy_button)
+        louie.connect(self.set_normal, Button.clicked, normal_button)
+        louie.connect(self.set_hard, Button.clicked, hard_button)
         louie.connect(self.finish_up, Button.clicked, finished_button)
 
         nump_buttons = [two_p_button, three_p_button, four_p_button]
         self.nump_buttons.set_buttons(nump_buttons)
         self.nump_buttons.set_check_color(Color.white)
 
+        skill_buttons = [easy_button, normal_button, hard_button]
+        self.skill_buttons.set_buttons(skill_buttons)
+        self.skill_buttons.set_check_color(Color.white)
+
         nump_buttons[self.game_config.num_players-2].set_checked(True)
+        skill_buttons[1].set_checked(True)
 
         children = [label, two_p_button, three_p_button, four_p_button,
+                    label_spacer1,
+                    easy_button, normal_button, hard_button,
                     label_spacer, finished_button, label_spacer0]
         self.widget.create_ui(children)
 
@@ -88,8 +105,20 @@ class OptionsState(State):
     def set_4p(self):
         self.set_nump(4)
 
+    def set_easy(self):
+        self.set_skill(EasySkillLevel())
+
+    def set_normal(self):
+        self.set_skill(NormalSkillLevel())
+
+    def set_hard(self):
+        self.set_skill(HardSkillLevel())
+
     def finish_up(self):
         louie.send(OptionsState.finished, game_config=self.game_config)
 
     def set_nump(self, num_players):
         self.game_config.num_players = num_players
+
+    def set_skill(self, skill):
+        self.game_config.computer_skill = skill
