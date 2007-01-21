fee6325efac104c265508c71af7527e6644c7023
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2007-01-21 12:13:52 +0000

    Fix function mismatches caused by the switch to a unified game config.

--- Application.py
+++ Application.py
@@ -106,7 +106,7 @@ class Application:
 
     def restart(self):
         """ Start a new game. deprecated? """
-        self.playing_field.configure(self.next_game_config)
+        self.playing_field.configure(self.match)
         self.transition('prepare')
 
     def main(self):
