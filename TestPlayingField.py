2486569bb99e0d7f04f2d3e42e4d2307e953e2d2
grant.yoshida <grant.yoshida@070e1d1f-b724-0410-ab84-1f9a774d1bb0>
2007-01-21 08:57:01 +0000

    Add a playing field test.

---
+++ TestPlayingField.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Pitta-pitta-patta
+# Released under the GPL version 2.0 or later.
+#
+
+import pygame
+import louie
+import logging
+import sys
+from GameConfig import GameConfig
+from Match import Match
+from PlayingField import PlayingField
+from Color import Color
+
+def main():
+    """ Tests just the game over screen. """
+    pygame.init()
+    screen = pygame.display.set_mode((850, 700), pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+    match = Match()
+    game_config = GameConfig()
+    playing_field = PlayingField(game_config)
+    playing_field.configure(match)
+    a = playing_field
+
+    logging.basicConfig(level=logging.DEBUG)
+    def dump(x):
+        logging.debug(x)
+
+    def game_over():
+        dump('Game over')
+
+    louie.connect(game_over, PlayingField.game_over)
+
+    while True:
+        pygame.time.wait(30)
+
+        for event in pygame.event.get():
+            a.handle(event)
+
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
+
+        a.update()
+        screen.fill(Color.bright_green)
+        a.draw(screen)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
